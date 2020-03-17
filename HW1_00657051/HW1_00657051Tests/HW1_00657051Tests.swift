bac4045d748ed0f2bca69d59d9d2570bcd3f8491
skywind121 <46217270+skywind121@users.noreply.github.com>
2020-03-17 10:05:01 -0700

    TestVer 0.01

---
+++ HW1_00657051/HW1_00657051Tests/HW1_00657051Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HW1_00657051Tests.swift
+//  HW1_00657051Tests
+//
+//  Created by SW on 2020/3/17.
+//  Copyright © 2020 00657051. All rights reserved.
+//
+
+import XCTest
+@testable import HW1_00657051
+
+class HW1_00657051Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
